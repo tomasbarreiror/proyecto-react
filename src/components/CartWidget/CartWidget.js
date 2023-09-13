dbcbe7d55ad73d8c22b6be6bbd3d7fccf59d5726
tomasbarreiror <tomasbarreiror@gmail.com>
2023-09-13 18:44:40 -0300

    first commit

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+import carrito from './assets/carrito.png'
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src={carrito} alt="cart-widget"/>
+        </div>
+    )
+}
+
+export default CartWidget;
