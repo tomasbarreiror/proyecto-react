cc7404f7a6bff4d54b14ef75bd26bc74ff8065ce
tomasbarreiror <tomasbarreiror@gmail.com>
2023-10-25 21:29:52 -0300

    Bug de imagenes solucionadas, falta arreglar el BuildPc

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -6,7 +6,7 @@ const Item = ({id, nombre, precio, img}) => {
     return (
         <div className="product">
             <h2>{nombre}</h2>
-            <img src={img} alt={nombre}/>
+            <img src={`${process.env.PUBLIC_URL}/${img}`} alt={nombre}/>
             <p>Precio: {precio} USD</p>
             <Link to={`/productos/${id}`} className="details">Ver detalle</Link>
         </div>
--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APY_KEY,
+    apiKey: "process.env.REACT_APP_API_KEY",
     authDomain: "proyecto-react-23214.firebaseapp.com",
     projectId: "proyecto-react-23214",
     storageBucket: "proyecto-react-23214.appspot.com",
